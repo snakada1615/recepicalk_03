137daf505d34ee952ac18cbde66c960a2ae75a69
shunichi <khf04440@nifty.com>
2022-03-24 23:42:25 +0900

    nutritionBar.vue終わり

---
+++ components/organisms/nutritionBar.vue
@@ -0,0 +1,132 @@
+<template>
+  <b-container class="my-0">
+    <b-row class="my-0" align-v="center">
+      <b-col :cols="colWidthFirst" class="px-0 d-flex justify-content-center">
+        <div>{{ cropName }}</div>
+      </b-col>
+      <b-col :cols="colWidthSecond" class="px-0 d-flex justify-content-center">
+        <div v-show="showDri" >{{ nutritionTarget }}</div>
+      </b-col>
+      <b-col class="px-0 d-flex justify-content-start">
+        <fa-rating :glyph="myicon"
+                   :item-size="20"
+                   :spacing="-5"
+                   :border-width="0"
+                   active-color="#FF3333"
+                   inactive-color="#cfcfcf"
+                   :increment="increment"
+                   :max-rating="max"
+                   :read-only="true"
+                   v-model="rating">
+        </fa-rating>
+        <div v-show="showReset" class="mx-2"><a href="#" @click.prevent="rating = 0">Reset</a></div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+
+<script>
+  import {FaRating} from 'vue-rate-it';
+  import align_justify from 'vue-rate-it/glyphs/align-justify';
+  import stop from 'vue-rate-it/glyphs/stop';
+  import navicon from 'vue-rate-it/glyphs/navicon';
+
+  export default {
+    components: {
+      FaRating
+    },
+    props: {
+      /**
+       * show reset button
+       */
+      showReset: {
+        type: Boolean,
+        default: false,
+      },
+      /**
+       * show number next to barchart
+       */
+      showDri: {
+        type: Boolean,
+        default: true,
+      },
+      /**
+       * actual score shown on barchart (relative value between [0, max])
+       */
+      rating: {
+        type: Number,
+        default: 5
+      },
+      /**
+       * max value of barchart (relative value)
+       */
+      max: {
+        type: Number,
+        default: 10
+      },
+      /**
+       * increment shown on the barchart
+       */
+      increment: {
+        type: Number,
+        default: 0.1
+      },
+      /**
+       * icon ID used for barchart renderig
+       */
+      iconNum: {
+        type: Number,
+        default: 1
+      },
+      /**
+       * label shown at left side of barchart
+       */
+      cropName: {
+        type: String,
+        required: true
+      },
+      /**
+       * absolute value equivalent to max
+       */
+      nutritionTarget: {
+        type: Number,
+        default: 0
+      },
+      colWidthFirst: {
+        type: Number,
+        default: 3
+      },
+      colWidthSecond: {
+        type: Number,
+        default: 2
+      }
+    },
+    data() {
+      return {
+        myicon: '', // declare the icon
+      }
+    },
+    created() {
+      this.myicon = stop
+    },
+    methods: {
+      setIcon(index) {
+        switch (index) {
+          case 1:
+            this.myicon = navicon
+            break;
+          case 2:
+            this.myicon = apple
+            break;
+          case 3:
+            this.myicon = ThumbsUp
+            break;
+          default:
+            this.myicon = navicon
+            break;
+        }
+      }
+    },
+  }
+</script>
--- pages/test02.vue
+++ pages/test02.vue
@@ -1,50 +1,37 @@
 <template>
   <b-container bg-variant="light" border-variant="primary">
-    <recepi-table
-      :items="cropList"
-      head-row-variant="success"
-      table-variant="light"
-      foot-row-variant="light"
-      @rowClick="onRecepiclick"
-      @update:items="cropList=JSON.parse(JSON.stringify($event))"
-    >
-    </recepi-table>
-    <div style="color: #3b8070">clicked item</div>
-    Name: {{clickedList.Name}}, Group: {{clickedList.Group}}, Energy: {{clickedList.En}}, Protein: {{clickedList.Pr}}, Vit-A: {{clickedList.Va}}, Iron: {{clickedList.Fe}}, Weight: {{clickedList.Wt}}
-    <div/>
-    <div style="color: #3b8070">add item</div>
-    <ul>
-      <li v-for="item in candidate">
-        <b-button @click="addData(item)" size="sm" variant="info">add</b-button> Name: {{item.Name}}, Group: {{item.Group}}, Energy: {{item.En}}, Protein: {{item.Pr}}, Vit-A: {{item.Va}}, Iron: {{item.Fe}}, Weight: {{item.Wt}}
-      </li>
-    </ul>
+    <nutrition-bar
+      cropName="Iron"
+      :iconNum=3
+      :max="10"
+      :nutritionTarget="test"
+      :rating="rate"
+      :showDri="ifShow"
+      :show-reset="ifReset"
+    ></nutrition-bar>
+    <b-form-input vtrue-model.number="test" type="number" placeholder="nutritionTarget"></b-form-input>
+    <p>rating:{{rate}}</p>
   </b-container>
 </template>
 <script>
-  import recepiTable from "../components/organisms/recepiTable";
+  import nutritionBar from "../components/organisms/nutritionBar";
+  import {setDigit} from "@/plugins/helper"
+
   export default {
     components:{
-      recepiTable
+      nutritionBar
     },
     data(){
-      return {
-        candidate:[
-          {id:"1", Group: "grain", Name: "taro", En: "25", Pr: "5", Va: "109", Fe: "13", Wt: "196"},
-          {id:"2", Group: "meat", Name: "pork", En: "15", Pr: "9", Va: "58", Fe: "31", Wt: "208"}
-        ],
-        cropList:[],
-        clickedList:{}
+      return{
+        test:90,
+        ifShow: true,
+        ifReset: true
       }
     },
-    methods:{
-      onRecepiclick(rec){
-        console.log(rec)
-        this.clickedList = rec
-      },
-      addData(val){
-        this.cropList.push(val)
+    computed: {
+      rate:function(){
+        return (this.test/100)*10
       }
     }
   }
-
 </script>
