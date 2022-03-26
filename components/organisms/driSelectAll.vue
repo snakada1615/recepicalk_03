90b87878235861bed92b1bd1a6cb87437eda4632
snakada1615 <khf04440@nifty.com>
2022-03-27 08:35:57 +0900

    firebase setup complete

--- components/organisms/driSelectAll.vue
+++ components/organisms/driSelectAll.vue
@@ -46,35 +46,51 @@
       leftRightSwitch
     },
     props: {
+      /**
+       * driPopulationsの切替スイッチ（singleモード/multiモード）
+       */
       targetSwitch:{
         type:Boolean,
         default: true
       },
+      /**
+       * driPopulations.countの上限値
+       */
       max: {
         type: Number,
         default: 1000
       },
+      /**
+       * ターゲットグループの構成：v-modelで使用
+       *  [{ id: 1, count: 1}, { id: 2, count: 5}, { id: 3, count: 0}]
+       */
       driPopulations: {
         type: Array,
         required: true,
-        // [{ id: 1, count: 1}, { id: 2, count: 5}, { id: 3, count: 0}]
       },
+      /**
+       *  driのデータセット
+       *   ex.
+       *          [{
+       *            En: "1088.0",
+       *            Fe: "5.8",
+       *            max_vol: "900",
+       *            Name: "child 6-23 month",
+       *            Pr: "11.65",
+       *            Va: "400.0",
+       *            id: 0
+       *           }],
+       */
       driItems: {
         type: Array,
         required: true,
-        // ex.
-        // [{
-        //   En: "1088.0",
-        //   Fe: "5.8",
-        //   max_vol: "900",
-        //   Name: "child 6-23 month",
-        //   Pr: "11.65",
-        //   Va: "400.0",
-        //   id: 0
-        //  }],
       }
     },
     methods: {
+      /**
+       * driPopulationsの更新時に値を親コンポーネントに通知
+       * @param val
+       */
       updateNutrition(val) {
         const res = JSON.parse(JSON.stringify(val))
         this.$emit('changeNutritionValue', res)
