91003e999726277da6b0b9ee4da449dfaf58aadd
snakada1615 <khf04440@nifty.com>
2022-04-06 18:27:11 +0900

    reload/page遷移のコントロール

--- pages/test01.vue
+++ pages/test01.vue
@@ -1,11 +1,21 @@
 <template>
-  <b-container >
-    halo
+  <b-container>
+    Hi!
+    <reload-warning :has-changed="hasChanged"/>
   </b-container>
 </template>
 
 <script>
+import reloadWarning from "../components/atoms/reloadWarning";
 
 export default {
+  data(){
+    return {
+      hasChanged: true
+    }
+  },
+  components:{
+    reloadWarning
+  },
 }
 </script>
